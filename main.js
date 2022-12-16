66fb4146f4a125590af150e099990ec67ee1f794
whitehatsoham <100528214+whitehatsoham@users.noreply.github.com>
2022-12-16 17:44:53 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX+" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.rightWrist.y;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + LeftWristX +" rightWristX = "+ rightWristX + " difference = " +difference);
+
+    }
+}
+
+function draw() {
+    background('#969A97')
+
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square will be =" + difference +"px"
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, 100);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
